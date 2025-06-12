a49f4cdbdd7de0ce068ca0404a69c4d97577a31f
Kirill Serebryannyy <sincerely.manny@gmail.com>
2025-06-12 14:57:45 +0300

    Add empty list placeholder and search bar to tracker index

--- YP_Tracker/Tracker/IndexViewController.swift
+++ YP_Tracker/Tracker/IndexViewController.swift
@@ -4,7 +4,8 @@ final class TrackerIndexViewController: UIViewController {
   private lazy var leftNavigationItemButton = {
     let button = UIBarButtonItem(
       image: UIImage(
-        systemName: "plus", withConfiguration: UIImage.SymbolConfiguration(pointSize: 24)),
+        systemName: "plus",
+        withConfiguration: UIImage.SymbolConfiguration(pointSize: 20, weight: .semibold)),
       style: .plain, target: self, action: nil)
 
     return button
@@ -23,6 +24,55 @@ final class TrackerIndexViewController: UIViewController {
     return UIBarButtonItem(customView: button)
   }()
 
+  private lazy var searchBar: UISearchBar = {
+    let searchBar = UISearchBar()
+    searchBar.placeholder = "Поиск"
+    searchBar.searchTextField.layer.cornerRadius = 8
+    searchBar.searchTextField.layer.masksToBounds = true
+    searchBar.backgroundImage = UIImage()
+    return searchBar
+  }()
+
+  private lazy var emptyListPlaceholder: UIView = {
+    let view = UIView()
+    let container = UIView()
+    view.addSubview(container)
+    container.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      container.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      container.leadingAnchor.constraint(
+        equalTo: view.leadingAnchor),
+      container.trailingAnchor.constraint(
+        equalTo: view.trailingAnchor),
+    ])
+
+    let imageView = UIImageView(image: UIImage(named: "empty_list"))
+    container.addSubview(imageView)
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      imageView.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+      imageView.topAnchor.constraint(equalTo: container.topAnchor),
+      imageView.heightAnchor.constraint(equalToConstant: 80),
+      imageView.widthAnchor.constraint(equalToConstant: 80),
+    ])
+
+    let label = UILabel()
+    label.text = "Что будем отслеживать?"
+    label.textColor = UIColor.ypBlack.appearance(.light)
+    label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+    label.textAlignment = .center
+    container.addSubview(label)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+      label.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+      label.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+      label.bottomAnchor.constraint(equalTo: container.bottomAnchor),
+    ])
+
+    return view
+  }()
+
   override func viewDidLoad() {
     super.viewDidLoad()
     setupView()
@@ -32,6 +82,28 @@ final class TrackerIndexViewController: UIViewController {
     title = "Трекеры"
     navigationItem.leftBarButtonItem = leftNavigationItemButton
     navigationItem.rightBarButtonItem = rightNavigationItemButton
+
+    view.addSubview(searchBar)
+    searchBar.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      searchBar.leadingAnchor.constraint(
+        equalTo: view.layoutMarginsGuide.leadingAnchor, constant: -8),
+      searchBar.trailingAnchor.constraint(
+        equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 8),
+    ])
+
+    view.addSubview(emptyListPlaceholder)
+    emptyListPlaceholder.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      emptyListPlaceholder.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+      emptyListPlaceholder.leadingAnchor.constraint(
+        equalTo: view.layoutMarginsGuide.leadingAnchor),
+      emptyListPlaceholder.trailingAnchor.constraint(
+        equalTo: view.layoutMarginsGuide.trailingAnchor),
+      emptyListPlaceholder.bottomAnchor.constraint(
+        equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+    ])
 
   }
 }
